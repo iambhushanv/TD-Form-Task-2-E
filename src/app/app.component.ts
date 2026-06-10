f92a596340447cd309bffe6579899bdd231d66e4
iambhushanv <bhushanvarude777@gmail.com>
2026-06-10 12:23:57 +0530

    done

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,6 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild } from '@angular/core';
+import { NgForm } from '@angular/forms';
+import { MatSnackBar } from '@angular/material/snack-bar';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +9,59 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'TD-Form-Task-2-E';
+
+  @ViewChild('empForm') empForm !: NgForm
+
+  constructor(
+    private _matSnackBar : MatSnackBar
+  ){}
+  
+  leaveTypes = [
+    'Sick Leave',
+    'Casual Leave',
+    'Earned Leave',
+    'Maternity Leave',
+    'Paternity Leave',
+    'Bereavement Leave',
+    'Unpaid Leave',
+    'Compensatory Leave',
+    'Public Holiday',
+    'Study Leave'
+  ];
+
+  minToDate: string = '';
+
+  dateError: boolean = false;
+
+  onFromDateChange(form: NgForm) {
+  const fDate = form.value.fDate;
+  const tDate = form.value.tDate;
+
+  this.minToDate = fDate;
+
+  if (tDate && tDate < fDate) {
+    this.dateError = true;
+  } else {
+    this.dateError = false;
+  }
+}
+
+
+onAdd() {
+ 
+  if (this.empForm.invalid) {
+    console.log(this.empForm.value);
+    
+    this.empForm.form.markAllAsTouched()
+    return
+  }else{
+      this.empForm.reset()
+       this._matSnackBar.open('Leave Application is applied successfully !!!', 'close',{
+      duration : 3000,
+      horizontalPosition : 'left',
+      verticalPosition : 'top'
+    })
+    
+  }
+}
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,20 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import {MatSnackBarModule} from '@angular/material/snack-bar';
+import { FormsModule } from '@angular/forms';
+
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    BrowserAnimationsModule,
+    MatSnackBarModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
